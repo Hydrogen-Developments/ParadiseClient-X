0c7dd5484342109c57e0eb7c79c10c0990f53df2
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-07-10 09:59:09 +0530

    Update CloudSyncPacket.java

--- src/main/java/net/paradise_client/packet/CloudSyncPacket.java
+++ src/main/java/net/paradise_client/packet/CloudSyncPacket.java
@@ -1,7 +1,5 @@
 package net.paradise_client.packet;
 
-import com.google.common.io.ByteArrayDataOutput;
-import com.google.common.io.ByteStreams;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.codec.PacketCodec;
@@ -11,50 +9,30 @@ import net.minecraft.util.Identifier;
 
 import java.util.Objects;
 
-public record CloudSyncPacket(String username, String command) implements CustomPayload {
+public record CloudSyncPacket(String username, String command) implements CustomPayload.IdAware {
+
+    public static final CustomPayload.Id<CloudSyncPacket> ID =
+            new CustomPayload.Id<>(Identifier.of("plugin:cloudsync"));
 
     public static final PacketCodec<PacketByteBuf, CloudSyncPacket> CODEC =
             CustomPayload.codecOf(CloudSyncPacket::write, CloudSyncPacket::new);
 
-    public static final Identifier ID = new Identifier("plugin", "cloudsync");
-
-    /**
-     * Private constructor used for deserialization of the packet from a byte buffer.
-     *
-     * @param buf The buffer containing the serialized packet data.
-     */
     private CloudSyncPacket(PacketByteBuf buf) {
         this(buf.readString(), buf.readString());
     }
 
-    /**
-     * Sends the cloudsync payload packet to the server.
-     *
-     * @param playerName The unique identifier for the user or session.
-     * @param command The command to be executed in the proxy.
-     */
     public static void send(String playerName, String command) {
         Objects.requireNonNull(MinecraftClient.getInstance().getNetworkHandler())
                 .sendPacket(new CustomPayloadC2SPacket(new CloudSyncPacket(playerName, command)));
     }
 
-    /**
-     * Serializes the packet data into the provided byte buffer.
-     *
-     * @param buf The buffer to which the packet data will be written.
-     */
     public void write(PacketByteBuf buf) {
         buf.writeString(username);
         buf.writeString(command);
     }
 
-    /**
-     * Retrieves the ID of this custom payload packet.
-     *
-     * @return The ID representing this custom payload packet.
-     */
     @Override
-    public Identifier getId() {
+    public CustomPayload.Id<? extends CustomPayload> payloadId() {
         return ID;
     }
 }
